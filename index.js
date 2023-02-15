23759dc2743ed01cd9711300e53be1b069a40f54
Dali Han <ross4han@gmail.com>
2023-02-15 07:40:17 +0000

    badges & licenses

--- index.js
+++ index.js
@@ -43,6 +43,20 @@ const questions = [
     }
 ];
 
+// function to generate lilcenses & badges
+
+const badges = {
+    "MIT": "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)",
+    "Apache 2.0": "[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)",
+    "GPL 3.0": "[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)",
+    "None": "",
+};
+
+const generateLicenseBadge = (license) => {return badges[license]};
+
+// function to generate readme content
+
+
 // function to write README file
 function writeToFile(fileName, data) {
 }
